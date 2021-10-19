de88309c6522e30054611334157337ce578afd94
zloisupport <zloisupport@yandex.ru>
2021-10-19 14:47:09 +0600

    + 4.10 - 4.11

--- 4.10.py
+++ 4.10.py
@@ -26,7 +26,7 @@ num9 = list('111101111001111')
 
 nums = [num0, num1, num2, num3, num4, num5, num6, num7, num8, num9]
 
-tema =3 # learning num
+tema = 5  # learning num
 n_sensor = 15  # amount sensor
 weights = [0 for i in range(n_sensor)]  # zeroing weights
 
@@ -60,7 +60,7 @@ def increase(number):
 
 
 # Training
-n = 5  # number of lessons
+n = 100 # number of lessons
 for i in range(n):
     j = random.randint(0, 9)  # Generate random  number j 0..9
     r = perceptron(nums[j])  # Summator result (true,false)
@@ -72,7 +72,15 @@ for i in range(n):
         # If the adder said NO (this is not a 5), and j is  5. True
         if not r:
             increase(nums[tema])
-print(j)
-
-
 print(weights)
+print(j)
+print("O is 5", perceptron(num0))
+print("1 is 5", perceptron(num1))
+print("2 is 5", perceptron(num2))
+print("3 is 5", perceptron(num3))
+print("4 is 5", perceptron(num4))
+print("5 is 5", perceptron(num5))
+print("6 is 5", perceptron(num6))
+print("7 is 5", perceptron(num7))
+print("8 is 5", perceptron(num8))
+print("9 is 5", perceptron(num9))
--- 4.11.py
+++ 4.11.py
@@ -22,11 +22,18 @@ num8 = list('111101111101111')
 
 num9 = list('111101111001111')
 
-# List all num 0..9
+num51 = list('111100111000111')
+num52 = list('111100010001111')
+num53 = list('111100011001111')
+num54 = list('110100111001111')
+num55 = list('110100111001011')
+num56 = list('111100101001111')
+
+# List a11 num 0..9
 
 nums = [num0, num1, num2, num3, num4, num5, num6, num7, num8, num9]
 
-tema =3 # learning num
+tema = 3  # learning num
 n_sensor = 15  # amount sensor
 weights = [0 for i in range(n_sensor)]  # zeroing weights
 
@@ -60,7 +67,7 @@ def increase(number):
 
 
 # Training
-n = 5  # number of lessons
+n = 100000  # number of lessons
 for i in range(n):
     j = random.randint(0, 9)  # Generate random  number j 0..9
     r = perceptron(nums[j])  # Summator result (true,false)
@@ -72,7 +79,11 @@ for i in range(n):
         # If the adder said NO (this is not a 5), and j is  5. True
         if not r:
             increase(nums[tema])
-print(j)
+# print(j)
 
-
-print(weights)
+print("1 is 5", perceptron(num51))
+print("2 is 5", perceptron(num52))
+print("3 is 5", perceptron(num53))
+print("4 is 5", perceptron(num54))
+print("5 is 5", perceptron(num55))
+print("6 is 5", perceptron(num56))
